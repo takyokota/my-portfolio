03a3db4785ac248199b574475d2b1c4ac1183805
xl883 <14enduro26er@gmail.com>
2023-07-13 00:50:47 -0600

    change: margin for skillbar

--- src/assets/data/skills_data.js
+++ src/assets/data/skills_data.js
@@ -5,6 +5,7 @@ export const skills_data = [
   { skill: 'React JS', level: 90 },
   { skill: 'Node / Express JS', level: 80 },
   { skill: 'Python', level: 80 },
+  { skill: 'PHP', level: 60 },
   { skill: 'MongoDB / PostgreSQL', level: 70 },
   { skill: 'FireBase', level: 60 },
   { skill: 'PhotoShop', level: 60 },
